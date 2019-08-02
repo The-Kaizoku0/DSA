1a68f4cb8a742500f553dcde301a54789e4a2570
Rajeshwar Sharma <48466540+The-Kaizoku0@users.noreply.github.com>
2019-08-02 16:18:41 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -39,6 +39,30 @@ int maxsumsubseq(int a[],int n)
     return *max_element(l,l+n);
     
 }
+int lcs(string a, string b, int n ,int m)
+{
+    int l[n+1][m+1];
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 || j==0)
+            {
+                l[i][j]=0;
+            }
+            else if(a[i-1]==b[j-1])
+            {
+                l[i][j]=1+l[i-1][j-1];
+            }
+            else
+            {
+                l[i][j]=max(l[i-1][j],l[i][j-1]);
+            }
+        }
+    }
+    return l[n][m];    
+}
+
 int main() 
 { 
     int t;
@@ -48,11 +72,18 @@ int main()
         int n;
         cin>>n;
         int a[n];
+        string x;
+        string y;
+        int o=x.size();
+        int p=y.size();
+        cin>>x>>y;
+        
         for(int i=0;i<n;i++)
         {
             cin>>a[i];
         }
         cout << lis(a,n)<< endl;
         cout << maxsumsubseq(a,n) << endl;
+        cout <<lcs (x,y,o,p) <<end;l;
     }
 }
