10ef0c58c68a80877fd0a340937a9ff67aad7360
Rajeshwar Sharma <48466540+The-Kaizoku0@users.noreply.github.com>
2019-08-25 12:20:29 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -128,6 +128,106 @@ int cost(int n,int a[][n])
     }
     return maxi;
 }
+ 
+// coin change 
+int count( int S[], int m, int n ) 
+{ 
+    int i, j, x, y; 
+  
+    // We need n+1 rows as the table  
+    // is constructed in bottom up 
+    // manner using the base case 0 
+    // value case (n = 0) 
+    int table[n + 1][m]; 
+      
+    // Fill the enteries for 0 
+    // value case (n = 0) 
+    for (i = 0; i < m; i++) 
+        table[0][i] = 1; 
+  
+    // Fill rest of the table entries  
+    // in bottom up manner  
+    for (i = 1; i < n + 1; i++) 
+    { 
+        for (j = 0; j < m; j++) 
+        { 
+            // Count of solutions including S[j] 
+            x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0; 
+  
+            // Count of solutions excluding S[j] 
+            y = (j >= 1) ? table[i][j - 1] : 0; 
+  
+            // total count 
+            table[i][j] = x + y; 
+        } 
+    } 
+    return table[n][m - 1]; 
+}  
+
+// subset sum problem
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+string subsetsum(int a[], int n, int sum)
+{
+    int dp[n+1][sum+1];
+    memset(dp,0,sizeof(dp));
+    for(int i=0;i<=sum;i++)
+    {
+        dp[0][i]=0;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        dp[i][0]=1;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+            if(a[i-1]>j)
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
+            }
+        }
+    }
+    if(dp[n][sum])
+    {
+        return "YES";
+    }
+    else
+    {
+        return "NO";
+    }
+}
+
+int main()
+ {
+	int t;cin>>t;
+	while(t--)
+	{
+	int n;cin>>n;
+	int a[n];
+	int sum=0;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	    sum=sum+a[i];
+	}
+	if(sum%2==0)
+	{cout << subsetsum(a,n,sum/2) << endl;}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+	}
+ }
+
+
 int main() 
 { 
     int t;
