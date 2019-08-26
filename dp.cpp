4f1637a5c154162a190978ea5febf548e72b38c5
Rajeshwar Sharma <48466540+The-Kaizoku0@users.noreply.github.com>
2019-08-27 02:10:20 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -199,6 +199,32 @@ string subsetsum(int set[], int n, int sum)
     }
 }
 
+//perfect sum, give no. of subset with given sum
+int perfectsum(int a[],int n, int sum)
+{
+    int dp[n+1][sum+1];
+    for(int j=1;j<=sum;j++)
+    {
+        dp[0][j]=0;
+    }
+    for(int i=0;i<=n;i++)
+    {
+        dp[i][0]=1;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+                dp[i][j]=dp[i-1][j];
+                if(j>=a[i-1])
+                {
+                    dp[i][j]+=dp[i-1][j-a[i-1]];
+                }
+        }
+    }
+    return dp[n][sum];
+}
+
 int main() 
 { 
     int t;
